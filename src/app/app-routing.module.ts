266ca0671cab4ed3f719952597f83820e571cf80
LedyaBakloug <ledya.beshara@yahoo.com>
2023-10-18 11:01:07 -0400

    add components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { UserSignupComponent } from './components/user-signup/user-signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'Home', component:HomeComponent},
+  {path:'Login', component:UserLoginComponent},
+  {path:'Signup', component:UserSignupComponent},
+  {path:'Admin', component:AdminComponent},
+  {path:'Customer', component:CustomerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { ButtonComponent } from './components/button/button.component';
+import { UsersComponent } from './components/users/users.component';
+import { UserLoginComponent } from './components/user-login/user-login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { UserSignupComponent } from './components/user-signup/user-signup.component';
+import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ButtonComponent,
+    UsersComponent,
+    UserLoginComponent,
+    UserSignupComponent,
+    HomeComponent,
+    AdminComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
